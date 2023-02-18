3498f4055b64f75705b4e52da5a1bdf3d6e062e9
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-02-19 00:17:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES1UG1UG20CS074\n";
+  cout << "PES1UG20CS074\n";
   return 0;
 }
